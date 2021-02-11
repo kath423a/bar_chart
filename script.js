489beb116b92d09890735f5676236b99e72da088
Kathrine <kath423a@stud.kea.dk>
2021-02-11 14:01:16 +0100

    simple functions added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+const model = [6, 2, 4, 5, 30, 22, 1, 7, 19, 22, 5, 11, 15, 8, 27, 24, 14, 9, 3, 16, 26, 17, 27, 6, 12, 18, 3, 0, 10, 20, 14, 29, 10, 13, 2, 11, 23, 9, 12, 23];
+
+function init() {}
+
+function loop() {}
+
+function getNumberOfCustomers() {}
+
+function displayData() {}
+
+function modifyModel() {}
