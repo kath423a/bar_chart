a82e53d042969fb999af098c0da46b832aece4b8
Kathrine <kath423a@stud.kea.dk>
2021-02-11 14:56:46 +0100

    bars virker ikke:O

--- script.js
+++ script.js
@@ -4,12 +4,36 @@ document.addEventListener("DOMContentLoaded", init);
 
 const model = [6, 2, 4, 5, 30, 22, 1, 7, 19, 22, 5, 11, 15, 8, 27, 24, 14, 9, 3, 16, 26, 17, 27, 6, 12, 18, 3, 0, 10, 20, 14, 29, 10, 13, 2, 11, 23, 9, 12, 23];
 
-function init() {}
+function init() {
+  console.log("init");
 
-function loop() {}
+  loop();
+}
 
-function getNumberOfCustomers() {}
+function loop() {
+  console.log("loop");
 
-function displayData() {}
+  getNumberOfCustomers();
+}
 
-function modifyModel() {}
+function getNumberOfCustomers() {
+  console.log("getNumberOfCustomers");
+
+  //FAKE gives a completely random number
+  return Math.floor(Math.random() * 32);
+
+  displayData();
+}
+
+function displayData() {
+  console.log("displayData");
+
+  //Add the current number of customers to the bar-chart:
+  const queueSize = getNumberOfCustomers();
+
+  modifyModel();
+}
+
+function modifyModel() {
+  console.log("modifyModel");
+}
